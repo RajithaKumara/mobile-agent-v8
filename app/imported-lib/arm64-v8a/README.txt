5bc199a4476da0223b82b3d7c37c72bed76d93ed
Rajitha Kumara <tmrkumara.15@cse.mrt.ac.lk>
2019-04-09 22:09:52 +0530

    Update Node.js to 8.15.1 only for arm,arm64
    * add libnode.so to arm64-v8a ABI
    
    * Add libnode.so from Node.js 8.15.1 LTS
    
    * Get v8 from Node 8.15.1
    
    * Update Node.js to 8.15.1 only for arm,arm64

---
+++ app/imported-lib/arm64-v8a/README.txt
@@ -0,0 +1,3 @@
+node 8.15.1
+
+{"v8_target_cpu": "arm64", "is_cfi": false, "target_cpu": "arm64", "is_ubsan_vptr": false, "is_gcov_coverage": false, "dcheck_always_on": false, "is_component_build": false, "is_asan": false, "is_debug": false, "v8_enable_i18n_support": false, "is_msan": false, "is_tsan": false, "v8_use_snapshot": false}
--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,19 +1,13 @@
 #include <jni.h>
 #include <string>
+#include <cstdlib>
+#include <android/log.h>
+#include <node.h>
 
 #include <libplatform/libplatform.h>
 #include <v8.h>
 
-
-//#include <EGL/egl.h>
-//#include <GLES/gl.h>
-
-
-//#include <android/sensor.h>
-//#include <android/log.h>
-//#include <android_native_app_glue>
-//#include <android/window.h>
-//#include <node.h>
+#define APPNAME "MobileAgentCPP"
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_exprograma_mobile_mobileagentcpp_MainActivity_stringFromJNI(
@@ -51,18 +45,138 @@ Java_com_exprograma_mobile_mobileagentcpp_MainActivity_stringFromJNI(
 #else
 #define ABI "unknown"
 #endif
-
-    std::string v8Ver = v8::V8::GetVersion();
-    std::string hello = "Hello from JNI C++ ! Compiled with ABI " ABI ". V8 version: "+v8Ver;
+    std::string hello = "Hello from JNI C++ ! Compiled with ABI " ABI "";
     return env->NewStringUTF(hello.c_str());
 }
 
 extern "C" JNIEXPORT jint JNICALL
-Java_com_exprograma_mobile_mobileagentcpp_MainActivity_stringFromCPP(JNIEnv *env, jobject /* this */) {
-    std::string hello = "Hello from C++";
-//    node::Start();
+Java_com_exprograma_mobile_mobileagentcpp_MainActivity_startNodeWithArguments(
+        JNIEnv *env,
+        jobject /* this */,
+        jobjectArray arguments) {
 
-//    (*env)->NewStringUTF("Hello from JNI ! Compiled with ABI ");
-//    return env->NewStringUTF(hello.c_str());
-    return env->GetVersion();
+    jsize argument_count = env->GetArrayLength(arguments);
+
+    int c_arguments_size = 0;
+    for (int i = 0; i < argument_count ; i++) {
+        c_arguments_size += strlen(env->GetStringUTFChars((jstring)env->GetObjectArrayElement(arguments, i), 0));
+        c_arguments_size++; // for '\0'
+    }
+
+    char* args_buffer = (char*) calloc(c_arguments_size, sizeof(char));
+
+    char* argv[argument_count];
+
+    char* current_args_position = args_buffer;
+
+    for (int i = 0; i < argument_count ; i++)
+    {
+        const char* current_argument = env->GetStringUTFChars((jstring)env->GetObjectArrayElement(arguments, i), 0);
+
+        strncpy(current_args_position, current_argument, strlen(current_argument));
+
+        argv[i] = current_args_position;
+
+        current_args_position += strlen(current_args_position) + 1;
+    }
+
+    int node_result = node::Start(argument_count, argv);
+    free(args_buffer);
+
+    return jint(node_result);
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_exprograma_mobile_mobileagentcpp_MainActivity_runNativeV8FromSnapshot(JNIEnv *env, jobject instance,
+                                                                               jobjectArray arguments) {
+
+    jsize argument_count = env->GetArrayLength(arguments);
+
+    int c_arguments_size = 0;
+    for (int i = 0; i < argument_count; i++) {
+        c_arguments_size += strlen(env->GetStringUTFChars((jstring) env->GetObjectArrayElement(arguments, i), 0));
+        c_arguments_size++; // for '\0'
+    }
+
+    char *args_buffer = (char *) calloc(c_arguments_size, sizeof(char));
+
+    char *argv[argument_count];
+
+    char *current_args_position = args_buffer;
+
+    for (int i = 0; i < argument_count; i++) {
+        const char *current_argument = env->GetStringUTFChars((jstring) env->GetObjectArrayElement(arguments, i), 0);
+
+        strncpy(current_args_position, current_argument, strlen(current_argument));
+
+        argv[i] = current_args_position;
+
+        current_args_position += strlen(current_args_position) + 1;
+    }
+
+//    jclass clazz = env->FindClass("com/exprograma/mobile/mobileagentcpp/JNI");
+//    jmethodID getLoc = env->GetStaticMethodID(clazz, "getExternalStorageDir", "()Ljava/lang/String;");
+//    jobject result = env->CallStaticObjectMethod(clazz, getLoc);
+//    const char *str = env->GetStringUTFChars((jstring) result, NULL);
+//    __android_log_print(ANDROID_LOG_WARN, APPNAME, "getExternalStorageDir: %s", str);
+
+    __android_log_print(ANDROID_LOG_WARN, APPNAME, "runNativeV8FromSnapshot: %s\n", argv[0]);
+
+    v8::V8::InitializeICU();
+    v8::V8::InitializeExternalStartupData(argv[0]);
+    v8::Platform *platform = v8::platform::CreateDefaultPlatform();
+    v8::V8::InitializePlatform(platform);
+    v8::V8::Initialize();
+
+    // Create a new Isolate and make it the current one.
+    v8::Isolate::CreateParams create_params;
+    create_params.array_buffer_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+    v8::Isolate *isolate = v8::Isolate::New(create_params);
+    {
+        v8::Isolate::Scope isolate_scope(isolate);
+        // Create a stack-allocated handle scope.
+        v8::HandleScope handle_scope(isolate);
+        // Create a new context.
+        v8::Local<v8::Context> context = v8::Context::New(isolate);
+        // Enter the context for compiling and running the hello world script.
+        v8::Context::Scope context_scope(context);
+        // Create a string containing the JavaScript source code.
+        v8::Local<v8::String> source = v8::String::NewFromUtf8(isolate, "'hello'+'hello'", v8::NewStringType::kNormal).ToLocalChecked();
+        // Compile the source code.
+        v8::Local<v8::Script> script = v8::Script::Compile(context, source).ToLocalChecked();
+        // Run the script to get the result.
+        v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+        // Convert the result to an UTF8 string and print it.
+        v8::String::Utf8Value utf8(result);
+
+        printf("%s\n", *utf8);
+
+        __android_log_print(ANDROID_LOG_WARN, APPNAME, "printf: %s\n", *utf8);
+
+        //Call to js function
+        v8::Handle<v8::Object> global = context->Global();
+        v8::Handle<v8::Value> function = global->Get(v8::String::NewFromUtf8(isolate, "addStr"));
+
+        if (function->IsFunction()) {
+            v8::Handle<v8::Function> func = v8::Handle<v8::Function>::Cast(function);
+            int func_argc = 2;
+            v8::Handle<v8::Value> func_argv[func_argc];
+            func_argv[0] = v8::String::NewFromUtf8(isolate, "Hello");
+            func_argv[1] = v8::String::NewFromUtf8(isolate, ", world!");
+            v8::Handle<v8::Value> func_result = func->Call(global, func_argc, func_argv);
+
+            v8::String::Utf8Value func_utf8(func_result);
+
+            __android_log_print(ANDROID_LOG_WARN, APPNAME, "printf: %s\n", *func_utf8);
+        } else {
+            __android_log_print(ANDROID_LOG_WARN, APPNAME, "printf: %s\n", "Not a function");
+        }
+    }
+
+    // Dispose the isolate and tear down V8.
+    isolate->Dispose();
+    v8::V8::Dispose();
+    v8::V8::ShutdownPlatform();
+
+    return env->NewStringUTF("Hello from JNI C++");
 }
